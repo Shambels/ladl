b33ac5e65f65b8bb2fc63d28e103eab130ff1766
Pinchett-i <sebastien.pinchetti@blue-planet.be>
2023-11-19 23:47:35 +0100

    Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(_resource)
+    if current_user.customer_representative?
+      customer_path(current_user.customer)
+    else
+      session['user_return_to'] || root_path
+    end
+  end
+
+  def user_not_authorized
+    flash[:alert] = t('flash.errors.unauthorized')
+    redirect_to(request.referer || root_path)
+  end
 end
